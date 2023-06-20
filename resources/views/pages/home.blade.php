68af267d73dd3d75da8b20f42a0a9c7a5e1f16da
B2014738 <ghib2014738@student.ctu.edu.vn>
2023-06-20 17:26:51 +0700

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+
+<div class="footer-bottom">
+            <div class="container">
+                <div class="inner">
+                    <div class="row">
+                        <div class="col-12">
+                            <div>
+                            
+                                <p><i class="fa-sharp fa-regular fa-location-dot"></i> Số 179, đường Nguyễn Văn Cừ, P. An Khánh, Q. Ninh Kiều, TP. Cần Thơ</p>
+                            </div>
+                            <div>
+                                <p>Email: bmtinhoc@ctump.edu.vn</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ resources/views/pages/slide.blade.php
@@ -0,0 +1,30 @@
+    <!-- Start Hero Area -->
+    <section class="hero-area">
+        <div class="hero-slider">
+            <!-- Single Slider -->
+            <div class="hero-inner overlay" style="background-image:url({{url('images/slide/1.jpg')}})">
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-lg-8 offset-lg-2 col-md-12 co-12">
+                            <div class="home-slider">
+                                <div class="hero-text">
+                                    <h5 class="wow fadeInUp" data-wow-delay=".3s">Start to Learning Today</h5>
+                                    <h1 class="wow fadeInUp" data-wow-delay=".5s">Excellent And Friendly <br> Faculty Members</h1>
+                                    <p class="wow fadeInUp" data-wow-delay=".7s">Lorem Ipsum is simply dummy text of the
+                                        printing and typesetting <br> industry. Lorem Ipsum has been the industry's
+                                        standard
+                                        <br>dummy text ever since an to impression.</p>
+                                    <div class="button wow fadeInUp" data-wow-delay=".9s">
+                                        <a href="about-us.html" class="btn">Learn More</a>
+                                        <a href="courses-grid.html" class="btn alt-btn">Our Courses</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/ End Single Slider -->   
+        </div>
+    </section>
+    <!--/ End Hero Area -->
